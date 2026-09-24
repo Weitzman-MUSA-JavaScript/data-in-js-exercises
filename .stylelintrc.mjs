4207b15f0c37709b2d365822e94db9e8c47a60c8
Mjumbe Poe <mjumbewu@gmail.com>
2026-09-24 01:43:33 -0400

    Style linting fixes

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -2,7 +2,10 @@ export default {
   extends: 'stylelint-config-standard',
   ignoreFiles: ['examples/**/*.css'],
   rules: {
+    'color-hex-length': null,
     'declaration-empty-line-before': null,
     'declaration-block-no-redundant-longhand-properties': null,
+    'no-descending-specificity': null,
+    'shorthand-property-no-redundant-values': null,
   },
 };
